a74cf65a9992f4c8de62ad7399d21fba16b434d6
Jiaming123 <jlkhf@mail.umkc.edu>
2016-03-06 20:43:14 -0600

    Changed

--- app/src/main/java/jiaming/myschedule/MainActivity.java
+++ app/src/main/java/jiaming/myschedule/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import static junit.framework.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import junit.framework.Assert;
@@ -20,6 +22,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
 
 //more and more
 public class MainActivity extends ActionBarActivity implements OnClickListener, OnItemClickListener {
@@ -37,6 +41,8 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
         View prevButton = findViewById(R.id.button);
         prevButton.setOnClickListener(this);
 
+        readItems();
+
         ListView listView = (ListView)this.findViewById(R.id.listOfschedule);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , listItems);
         listView.setAdapter(adapter);
@@ -53,6 +59,7 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
         listItems.add(tv.getText().toString());
         // Notify adapter that underlying data structure changed
         adapter.notifyDataSetChanged();
+        writeItems();
     }
 
     // This is for selecting an item from the list
@@ -61,5 +68,31 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
         Intent intent = new Intent(MainActivity.this, PickerActivity.class);
         startActivity(intent);
     }
+
+    ////////////////save string data for Submittion
+    private void readItems () {
+        File filesDir = getFilesDir();
+        File scheduleFile = new File (filesDir, "schedule.txt");
+        try{
+            listItems = new ArrayList<String>(FileUtils.readLines(scheduleFile));
+        } catch (IOException e) {
+            listItems = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems () {
+        File fileDir = getFilesDir();
+        File scheduleFile = new File (fileDir, "schedule.txt");
+        try {
+            FileUtils.writeLines(scheduleFile, listItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ///////////////////我想问问你关于储存数据的问题，我现在问了问人，可以这个样子把条目储存猪了，但是我的
+    ///////////////////TimePicker 和DatePicker 完全不会储存，我想问问你有什么方法吗？
+
+
 }
 
